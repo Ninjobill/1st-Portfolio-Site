fedb7a726a6e8d7f88b325108f204d872d1133b9
Billy Todd <billy.todd50@yahoo.com>
2019-07-23 19:40:42 -0700

    add about me, tweak things in portfolio.

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,3 @@
-// $(document).ready(() => {
-// 	$('#navbar').hide()
-// });
-
 $(document).click(function(e) {
 	if (!$(e.target).is('.panel-body')) {
     	$('.collapse').collapse('hide');	    
--- JS/loader.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    const load_screen = document.getElementById("load-screen");
-    setTimeout(() => {
-        document.body.removeChild(load_screen);
-           
-    }, 1000);
-});
